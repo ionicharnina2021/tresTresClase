8a3135cceeea287c2e3ec404018b1128d8cf7e3f
programacion <>
2021-01-18 10:30:53 +0100

    implementado el nativo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,39 @@
-import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { Platform, ToastController } from '@ionic/angular';
-import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from "@ionic-native/geolocation/ngx";
+import { Platform, ToastController } from "@ionic/angular";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
+  uno:Geoposition;
+  ubicacion = { lat: 0, long: 0 };
   //En este proyecto ya tenemos instalado el geolocation
-  constructor() {}
- 
+  constructor(
+    private plat: Platform,
+    private geolocation: Geolocation,
+    public toast: ToastController
+  ) {}
+  localizar() {
+    this.plat
+      .ready()
+      .then(() => {
+        this.geolocation
+          .getCurrentPosition()
+          .then((info) => {
+            this.ubicacion.lat = info.coords.latitude;
+            this.ubicacion.long = info.coords.longitude;
+          })
+          .catch(async (error) => {
+            let toast=this.toast.create({
+              message:'error '+error,
+              duration:1000
+            });
+            (await toast).present();
+          });
+      })
+      .catch();
+  }
 }
